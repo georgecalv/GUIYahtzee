81748e36572f4697b1e3bbfa99a8d8d2452f3570
georgecalv <gcalvert@zagmail.gonzaga.edu>
2022-11-16 16:56:52 -0800

    vector of player objects

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Yahtzee {
     // runs ands displays game of yahtzee
     private int numDie;
@@ -5,6 +6,7 @@ public class Yahtzee {
     private int sideDie;
     private int numPlayers;
     private String[] playerNames;
+    private Vector<Player> playerVec;
     private Score score;
     private Die die;
 
@@ -14,12 +16,20 @@ public class Yahtzee {
         this.sideDie = sideDie;
         this.numPlayers = numPlayers;
         this.playerNames = playerNames;
-
         this.score = new Score(this.numDie, this.sideDie);
         this.die = new Die(this.sideDie);
 
+        createPlayers();
     }
+    // runs game
     public void playGame() {
-
+        // loop for each player until they want to end the game or all lines are gone
+        // another loop for how many rolls per player
+    }
+    // makes vector of player objects
+    public void createPlayers() {
+        for(int i = 0; i < this.numPlayers; i++) {
+            playerVec.add(new Player(playerNames[i], this.numDie, this.sideDie));
+        }
     }
 }
