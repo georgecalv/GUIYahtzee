bb22a8ad4bc06ce4a12f73a3257a36055852cb03
georgecalv <gcalvert@zagmail.gonzaga.edu>
2022-11-16 16:02:56 -0800

    demo of get config

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // calll initial display which gets the number die, number sides, number players
+        InitialDisplay id = new InitialDisplay();
+        id.Display();
     }
 }
--- MakePlayers.java
+++ MakePlayers.java
@@ -1,4 +1,19 @@
 public class MakePlayers {
     // asks for player names and creates each object along with their scorecards
     // starts game of yahtzee
+    private int numDie;
+    private int numTurns;
+    private int sideDie;
+    private int numPlayers;
+
+    public MakePlayers(int numDie, int numTurns, int sideDie, int numPlayers) {
+        this.numDie = numDie;
+        this.numTurns = numTurns;
+        this.sideDie = sideDie;
+        this.numPlayers = numPlayers;
+    }
+    public void getPlayers() {
+        System.out.println("numdie = " + this.numDie + " numTurns = " + this.numTurns + " sideDie = " + this.sideDie + " numPLayers = " + this.numPlayers);
+    }
+
 }
